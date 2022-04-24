4bce40f9b47ba76553807b46f900e7fc611aa31e
Maryam Abdool <maryamrabd@gmail.com>
2022-04-23 23:12:23 -0400

    Initial Commit

---
+++ Shared/Monitor_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monitor_MeApp.swift
+//  Shared
+//
+//  Created by Maryam Abdool on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Monitor_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
